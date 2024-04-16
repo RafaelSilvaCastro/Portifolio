f63acebfbe0e01e54b2e415fc4c1d01d31aabf6c
RafaelSilvaCastro <165667524+RafaelSilvaCastro@users.noreply.github.com>
2024-04-16 16:02:46 -0300

    Update carrinho.js

--- loja_produtos/loja_produtos_js/js/carrinho.js
+++ loja_produtos/loja_produtos_js/js/carrinho.js
@@ -1,6 +1,15 @@
 const removeProductButtons = document.getElementsByClassName("remove-product-button");
 for (var i = 0; i < removeProductButtons.length; i++){
-    removeProductButtons[i].addEventListener("click", function(){
-        console.log('clicou')
-    })
+    removeProductButtons[i].addEventListener("click", function(event){
+     event.target.parentElement.parentElement.remove();
+    });
+}
+
+
+const cartProduts = document.getElementsByClassName("cart-product");
+for(var i = 0; i < cartProduts.length; i++){
+    //console.log(cartProduts[i])
+    const productPrice = cartProduts[i].getElementsByClassName("cart-product-price")[0].innerHTML;
+    const productQuantity = cartProduts[i].getElementsByClassName("product-qtd-input")[0].value;
+
 }
