d52785943eb5c967c3d28c4b467bc5f5d2e64d8e
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-04-29 19:22:41 -0300

    Update main.py

--- jogo_rpg/main.py
+++ jogo_rpg/main.py
@@ -40,16 +40,57 @@ def gerar_npcs(n_npcs):
         
 def exibir_npcs():
     for npc in lista_npcs:
-        print(f"Nome: {npc['nome']} // Level: {npc['level']} // Dano: {npc['dano']} // HP: {npc['hp']} // EXP: {npc['exp']}")
+        exibir_npc(npc)
         
 #fim função exibir npcs
- 
+
+def exibir_npc(npc):
+    print(f"Nome: {npc['nome']} // Level: {npc['level']} // Dano: {npc['dano']} // HP: {npc['hp']} // EXP: {npc['exp']}")
+
+#fim da função exibir npc
+
+def exibir_player():
+    print(f"Nome: {player['nome']} // Level: {player['level']} // Dano: {player['dano']} // HP: {player['hp']} / {player['hp_max']}  // EXP: {player['exp']} / {player['exp_max']}")
+    
+#fim da função exibir player
+    
+def reset_player():
+    player['hp'] = player['hp_max']
+    
+#fim reset player
+    
+def reset_npc(npc):
+    npc['hp'] = npc['hp_max']
+    
+#fim reset npc
+
+def level_up():
+    if player['exp'] >= player['exp_max']:
+        player['level'] += 1
+        player['exp'] = 0
+        player['exp_max'] = player['exp_max'] * 2
+        player['hp_max'] += 20
+
+#fim level up
 
 def iniciar_batalha(npc):
     while player['hp'] > 0 and npc['hp'] > 0:  
         atacar_npc(npc)
         atacar_player(npc)
         exibir_info_batalha(npc)
+        
+        if player['hp'] > 0:
+            print(f"O {player['nome']} venceu e ganhou {npc['exp']} de EXP!")
+            player['exp'] += npc['exp']
+            exibir_player()
+        else:
+            print(f"O {npc['nome']} venceu!")
+            exibir_npc(npc)
+            
+        level_up()
+        reset_player()
+        reset_npc(npc)
+        
 #fim função iniciar batalha
 
 
@@ -69,7 +110,7 @@ def atacar_player(npc):
 def exibir_info_batalha(npc):
     print(f"Palyer: {player['hp']} / {player['hp_max']}")
     print(f"NPC: {npc['nome']} {npc['hp']} / {npc['hp_max']}")
-    print(20*"-")   
+    print(30*"-")   
 
 #fim função exibir info batalha
  
